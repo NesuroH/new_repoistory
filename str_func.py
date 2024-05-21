db7cfed0fdfeb20bbff0460b3a3cd1e05b21d6a4
Ivan Nikolaev <nikolaevcool07@gmail.com>
2024-05-21 22:41:25 +0500

    added new function_2

--- str_func.py
+++ str_func.py
@@ -1,2 +1,10 @@
 def new_text(value):
+    """
+    функция обращает все буквы в строке в заглавные
+    """
     return value.upper()
+
+
+def first_symbol(value):
+    """"делает каждую первую букву заглавной"""
+    return value.title()
